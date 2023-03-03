ac08529088175e23b28a0e68a0421a48e52bc075
Michal Sklenár <michal.sklenar@sklinet.com>
2023-03-03 23:04:07 +0100

    Update plugins.js

--- cms/config/plugins.js
+++ cms/config/plugins.js
@@ -20,23 +20,6 @@ module.exports = ({ env }) => ({
             playgroundAlways: true,
         },
     },
-    email: {
-        enabled: true,
-        provider: "nodemailer",
-        providerOptions: {
-            host: env("SMTP_HOST", "smtp.example.com"),
-            port: env("SMTP_PORT", 587),
-            auth: {
-                user: env("SMTP_USERNAME"),
-                pass: env("SMTP_PASSWORD"),
-            },
-            // ... any custom nodemailer options
-        },
-        settings: {
-            defaultFrom: "no-reply@boilerplate.sk",
-            defaultReplyTo: "no-reply@boilerplate.sk",
-        },
-    },
     "preview-button": {
         enabled: true,
         openTarget: "_blank",
